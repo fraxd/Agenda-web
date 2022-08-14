c47c5c40cf33ffe523dfa0238ca5c60c051b6f6b
fraxd <fraxd98@gmail.com>
2022-08-14 02:12:48 -0400

    first test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./gestion/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./gestion/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proyecto-agenda-web-a1177',
+    appId: '1:199579897908:web:bb841ef316e6a863e57a61',
+    storageBucket: 'proyecto-agenda-web-a1177.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAieADluIOPD4YB6B6ziT-n9ju7eO-9eVc',
+    authDomain: 'proyecto-agenda-web-a1177.firebaseapp.com',
+    messagingSenderId: '199579897908',
+    measurementId: 'G-JDBQNFT3NV',
+  },
   production: false
 };
 
